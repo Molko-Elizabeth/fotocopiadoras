04d1b3e17f7f417cb53c4efc1c8f362bf9baeda4
Molko-Elizabeth <84107356+Molko-Elizabeth@users.noreply.github.com>
2021-08-30 19:33:37 -0300

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,34 @@
+<?php
+// prueba.php
+//levanto datos del form
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$comentarios = $_POST['comentarios'];
+ //mail a mi mail
+ mail(
+     "maguigareal@gmail.com",
+     "mail desde web.com",
+     "$nombre te envio $comentario contactalo a $email",
+    "from:super sitio <info@superweb.com>"
+ );
+ //mail usuario
+ mail(
+    $email,"web.com recibimos tu consulta",
+    "recibimos tu consulta,te queremos sasass",
+    "from:super sitio <info@superweb.com>"
+ );
+ echo "mensaje enviado";
+
+
+
+
+
+// destinatario / asunto / cuerpo --- mandar x post
+/*mail(
+    "maguigareal@gmail.com",
+    "Probando form",
+    "Este es el mensaje",
+    "from:super sitio <info@superweb.com>"
+);*/
+
+?>
